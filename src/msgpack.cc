pgriess/node-msgpack

commit 6b4c80c3bb62860533fcee4256120543bd3ab1b1

generate less warnings on windows compilation

src/msgpack.cc
@@ -147,14 +147,14 @@ v8_to_msgpack(Handle<Value> v8obj, msgpack_object *mo, msgpack_zone *mz,
             mo->via.dec = d;
         } else if (d > 0) {
             mo->type = MSGPACK_OBJECT_POSITIVE_INTEGER;
-            mo->via.u64 = d;
+            mo->via.u64 = static_cast<uint64_t>(d);
         } else {
             mo->type = MSGPACK_OBJECT_NEGATIVE_INTEGER;
-            mo->via.i64 = d;
+            mo->via.i64 = static_cast<int64_t>(d);
         }
     } else if (v8obj->IsString()) {
         mo->type = MSGPACK_OBJECT_RAW;
-        mo->via.raw.size = DecodeBytes(v8obj, UTF8);
+        mo->via.raw.size = static_cast<uint32_t>(DecodeBytes(v8obj, UTF8));
         mo->via.raw.ptr = (char*) msgpack_zone_malloc(mz, mo->via.raw.size);
 
         DecodeWrite((char*) mo->via.raw.ptr, mo->via.raw.size, v8obj, UTF8);
@@ -178,7 +178,7 @@ v8_to_msgpack(Handle<Value> v8obj, msgpack_object *mo, msgpack_zone *mz,
 
 
         mo->type = MSGPACK_OBJECT_RAW;
-        mo->via.raw.size = Buffer::Length(buf);
+        mo->via.raw.size = static_cast<uint32_t>(Buffer::Length(buf));
         mo->via.raw.ptr = Buffer::Data(buf);
     } else {
         Local<Object> o = v8obj->ToObject();
@@ -216,10 +216,10 @@ msgpack_to_v8(msgpack_object *mo) {
             False();
 
     case MSGPACK_OBJECT_POSITIVE_INTEGER:
-        return Integer::NewFromUnsigned(mo->via.u64);
+        return Integer::NewFromUnsigned(static_cast<uint32_t>(mo->via.u64));
 
     case MSGPACK_OBJECT_NEGATIVE_INTEGER:
-        return Integer::New(mo->via.i64);
+        return Integer::New(static_cast<int32_t>(mo->via.i64));
 
     case MSGPACK_OBJECT_DOUBLE:
         return Number::New(mo->via.dec);
@@ -324,7 +324,7 @@ unpack(const Arguments &args) {
         try {
             msgpack_unpack_template->GetFunction()->Set(
                 msgpack_bytes_remaining_symbol,
-                Integer::New(Buffer::Length(buf) - off)
+                Integer::New(static_cast<int32_t>(Buffer::Length(buf) - off))
             );
             return scope.Close(msgpack_to_v8(&mo));
         } catch (MsgpackException e) {

Makefile
@@ -1,45 +1,0 @@
-OS = $(shell uname)
-CWD = $(shell pwd -P)
-NODE_WAF ?= node-waf
-NODE_SRC_DIR ?= $(HOME)/src/ry-node
-CFLAGS ?= -g -Wall
-CXXFLAGS ?= -g -Wall
-
-# We need to build position-independent code regardless of platform
-ifneq ($(OS),Darwin)
-	CFLAGS += -fPIC
-	CXXFLAGS += -fPIC
-endif
-
-# These variables are respected by waf if we export them
-export CFLAGS CXXFLAGS
-
-.PHONY: all msgpack tags
-
-all: msgpack
-
-msgpack: deps/msgpack/dist/lib/libmsgpack.a
-	cd src && \
-		$(NODE_WAF) configure && \
-		$(NODE_WAF) build
-
-# Build the msgpack library
-deps/msgpack/dist/lib/libmsgpack.a:
-	cd deps/msgpack && \
-		mkdir -p dist && \
-		./configure --enable-static --disable-shared \
-			--prefix=$(PWD)/deps/msgpack/dist && \
-		make && \
-		make install
-
-tags:
-	rm -f tags
-	find . -name '*.cc' -or -name '*.c' -or -name '*.cpp' -or -name '*.h' -or \
-		-name '*.hpp' -or -name '*.cpp' | ctags -L - -a
-	test -d $(NODE_SRC_DIR) && \
-		find $(NODE_SRC_DIR) -name '*.cc' -or -name '*.c' -or -name '*.h' | \
-			ctags -L - -a
-
-clean:
-	cd deps/msgpack && (make distclean || true)
-	rm -fr deps/msgpack/dist build

binding.gyp
@@ -29,11 +29,24 @@
 
 					}],
 					['OS=="win"', {
-						'msvs_settings': {
-							'VCCLCompilerTool': {
-								'CompileAs': 2
-							}
-						}
+						'configurations': {
+							'Debug': {
+								'msvs_settings': {
+									'VCCLCompilerTool': {
+										'CompileAs': '2',
+										'ExceptionHandling': '1',
+									},
+								},
+							},
+							'Release': {
+								'msvs_settings': {
+									'VCCLCompilerTool': {
+										'CompileAs': '2',
+										'ExceptionHandling': '1',
+									},
+								},
+							},
+						},
 					}]
 				]
 			},

deps/msgpack/msgpack.gyp
@@ -30,11 +30,24 @@
 
 					}],
 					['OS=="win"', {
-						'msvs_settings': {
-							'VCCLCompilerTool': {
-								'CompileAs': 2
-							}
-						}
+						'configurations': {
+							'Debug': {
+								'msvs_settings': {
+									'VCCLCompilerTool': {
+										'CompileAs': '2',
+										'ExceptionHandling': '1',
+									},
+								},
+							},
+							'Release': {
+								'msvs_settings': {
+									'VCCLCompilerTool': {
+										'CompileAs': '2',
+										'ExceptionHandling': '1',
+									},
+								},
+							},
+						},
 					}]
 				]
 			},

deps/msgpack/msgpack/pack_template.h
@@ -743,7 +743,7 @@ msgpack_pack_inline_func(_raw)(msgpack_pack_user x, size_t l)
 
 msgpack_pack_inline_func(_raw_body)(msgpack_pack_user x, const void* b, size_t l)
 {
-	msgpack_pack_append_buffer(x, (const unsigned char*)b, l);
+	msgpack_pack_append_buffer(x, (const unsigned char*)b, static_cast<unsigned int>(l));
 }
 
 #undef msgpack_pack_inline_func

commit abe2d184f5f4ae46adb84805c39fd681ea6fc2c6

This fixes Issue #42 and puts in at least one regression test for 64-bit integer values, which work in node.js up to about 2^54 and down to -2^54. May as well make a best effort to get them right.  This issue is due to a regression introduced in commits  6b4c80c and 0b4f361, which taken together always encode larger integers as doubles, even when they shouldn't be.

src/msgpack.cc
@@ -158,7 +158,7 @@ v8_to_msgpack(Handle<Value> v8obj, msgpack_object *mo, msgpack_zone *mz,
         mo->via.boolean = v8obj->BooleanValue();
     } else if (v8obj->IsNumber()) {
         double d = v8obj->NumberValue();
-        if (static_cast<double>(static_cast<int>(d)) != d) {
+        if (trunc(d) != d) {
             mo->type = MSGPACK_OBJECT_DOUBLE;
             mo->via.dec = d;
         } else if (d > 0) {
@@ -244,10 +244,15 @@ msgpack_to_v8(msgpack_object *mo) {
             False();
 
     case MSGPACK_OBJECT_POSITIVE_INTEGER:
-        return Integer::NewFromUnsigned(static_cast<uint32_t>(mo->via.u64));
+        // As per Issue #42, we need to use the base Number
+        // class as opposed to the subclass Integer, since
+        // only the former takes 64-bit inputs. Using the
+        // Integer subclass will truncate 64-bit values.
+        return Number::New(static_cast<double>(mo->via.u64));
 
     case MSGPACK_OBJECT_NEGATIVE_INTEGER:
-        return Integer::New(static_cast<int32_t>(mo->via.i64));
+        // See comment for MSGPACK_OBJECT_POSITIVE_INTEGER
+        return Number::New(static_cast<double>(mo->via.i64));
 
     case MSGPACK_OBJECT_DOUBLE:
         return Number::New(mo->via.dec);

test/test.js
@@ -29,6 +29,7 @@ testEqual(false);
 testEqual([1, 2, 3]);
 testEqual([1, 'abc', false, null]);
 testEqual({'a' : [1, 2, 3], 'b' : 'cdef', 'c' : {'nuts' : 'qqq'}});
+testEqual(0 -  Math.pow(2,31) - 1);
 
 // Make sure dates are handled properly
 var date = new Date();